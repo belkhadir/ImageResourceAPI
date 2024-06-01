634d721b158e0d3d84b8ac8ecd01c646b048298a
belkhadir <belkhadir.anas@gmail.com>
2024-06-01 12:27:20 +0100

    Initial Commit

---
+++ ImageResourceAPI/ImageResourceAPI.h
@@ -0,0 +1,18 @@
+//
+//  ImageResourceAPI.h
+//  ImageResourceAPI
+//
+//  Created by Belkhadir Anas on 1/6/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ImageResourceAPI.
+FOUNDATION_EXPORT double ImageResourceAPIVersionNumber;
+
+//! Project version string for ImageResourceAPI.
+FOUNDATION_EXPORT const unsigned char ImageResourceAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImageResourceAPI/PublicHeader.h>
+
+
